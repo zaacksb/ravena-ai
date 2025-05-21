064773a5a0baa681f2ec48bd982a633c4c7fc941
Fábio Henrique Araújo <contato@fabioharaujo.com.br>
2025-05-21 13:15:02 -0300

    Comando para buscar e enviar áudios do MyInstants

--- Test-mockup.js
+++ Test-mockup.js
@@ -390,8 +390,8 @@ async function runTests() {
     await new Promise(resolve => setTimeout(resolve, 5000));
     logger.info('sending');
 
-    await simulator.simulateTextMessage(testUser, testGroup, '$geotest');
-    //await simulator.simulateTextMessage(testUser, testGroup, '!geoguess -1.2614419 -60.4063437');
+    await simulator.simulateTextMessage(testUser, testGroup, '!audio-buscar fbi open up');
+    await simulator.simulateTextMessage(testUser, testGroup, '!audio-buscar fbi open up 9');
 
   
     //logger.info('joining');    
