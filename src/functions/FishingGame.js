0d6055c643d43e1edccfdf9ab614d23ed1652763
moothz <m.moothz@gmail.com>
2025-07-31 08:51:13 -0300

    Update FishingGame.js

--- src/functions/FishingGame.js
+++ src/functions/FishingGame.js
@@ -1840,8 +1840,8 @@ async function getFishingStats() {
 
     for (const userData of allUsersData) {
         totalFishCaught += userData.totalCatches || 0;
-        totalBaitsUsed += (userData.totalBaitsUsed || 0) + totalFishCaught*1.2;
-        totalTrashCaught += (userData.totalTrashCaught || 0) + (totalBaitsUsed - totalFishCaught);
+        totalBaitsUsed += (userData.totalBaitsUsed || 0);
+        totalTrashCaught += (userData.totalTrashCaught || 0);
 
         if (userData.biggestFish && userData.biggestFish.weight > heaviestFishEver.weight) {
             heaviestFishEver = {
@@ -1857,6 +1857,9 @@ async function getFishingStats() {
             };
         }
     }
+
+    totalBaitsUsed += Math.floor(totalFishCaught*1.2);
+    totalTrashCaught += (totalBaitsUsed - totalFishCaught);
 
     const totalLegendaryCaught = fishingData.legendaryFishes?.length || 0;
 
